9f95ec3b58e8aeb656569934d36b4d5a1fae10c9
Mikko Taipale <mikko.taipale2@gmail.com>
2022-09-14 18:15:21 +0300

    Hello function edited

--- toka.js
+++ toka.js
@@ -3,4 +3,10 @@ function sayHello()
     console.log("Jello from function");
 }
 
+function greetings(fname)
+{
+    console.log("Hello "+fname+" how are you?");
+}
+
 sayHello();
+greetings("Jaakko");

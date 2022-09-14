88323479c68cc5d5772889f8b8b943f3ce6fd233
Mikko Taipale <mikko.taipale2@gmail.com>
2022-09-14 18:08:04 +0300

    Hello function works

--- eka.js
+++ eka.js
@@ -1,2 +1,8 @@
 console.log("Hello World!");
 console.log("Terve");
+let x=10;
+for(let x=1; x<5; x++)
+{
+    console.log("Rivi "+x);
+}
+console.log("Loopin jälkeen x="+x);
---
+++ toka.js
@@ -0,0 +1,6 @@
+function sayHello()
+{
+    console.log("Jello from function");
+}
+
+sayHello();
